c3ad694d0eedfdb576023df271bf5c69dd2641f4
emoacht <emotom@pobox.com>
2014-12-02 12:49:46 +0900

    Adjusted train item appearance

---
+++ TokyoSubwayView/TokyoSubwayView.Shared/Views/Converters/TransparentBrushToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace TokyoSubwayView.Views.Converters
+{
+	public class TransparentBrushToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (!(value is SolidColorBrush))
+				return DependencyProperty.UnsetValue;
+
+			return (((SolidColorBrush)value).Color == Colors.Transparent)
+				? Visibility.Collapsed
+				: Visibility.Visible;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
